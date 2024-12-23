96af9c1eda0a301726fda59098ad7b8aafb73b0a
Jonas Paprotka <152098213+JonasPaprotka@users.noreply.github.com>
2024-12-23 16:54:48 +0100

    v0.0.1 (#2)

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const path = require('path');
 
 let mainWindow;
+let updateWindow;
 
-function createWindow() {
+function createMainWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -11,23 +13,50 @@ function createWindow() {
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             contextIsolation: true,
-            nodeIntegration: false
-        }
+            nodeIntegration: false,
+        },
     });
 
-    mainWindow.loadFile(path.join(__dirname, 'renderer/index.html'));
+    mainWindow.loadFile('renderer/index.html');
 
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
 }
 
-app.whenReady().then(() => {
-    createWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+function createUpdateWindow() {
+    updateWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        resizable: false,
+        frame: false,
+        webPreferences: {
+            contextIsolation: true,
+        },
     });
+
+    updateWindow.loadFile('renderer/update.html');
+}
+
+app.on('ready', () => {
+    if (app.isPackaged) { // Do not update when dev
+        autoUpdater.checkForUpdates();
+
+        autoUpdater.on('update-available', (info) => {
+            if (!updateWindow) createUpdateWindow();
+        });
+
+        autoUpdater.on('update-downloaded', (info) => {
+            autoUpdater.quitAndInstall();
+        });
+
+        autoUpdater.on('error', (error) => {
+            if (updateWindow) updateWindow.close();
+            createMainWindow();
+        });
+    } else {
+        createMainWindow();
+    }
 });
 
 app.on('window-all-closed', () => {
--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,1 +1,14 @@
+fetch('titlebar.html')
+.then(response => response.text())
+.then(html => {
+  document.getElementById('titlebar-container').innerHTML = html;
 
+  document.getElementById('minimize').addEventListener('click', () => {
+    window.electronAPI.minimize();
+  });
+
+  document.getElementById('close').addEventListener('click', () => {
+    window.electronAPI.close();
+  });
+})
+.catch(error => console.error('Error loading title bar:', error));
